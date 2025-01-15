20b070e807d5811e6c01ac57376146fca93363d0
root <lachie.duncan12@gmail.com>
2025-01-15 00:34:09 +0000

    Added __name__ == "__main__"

--- python-import_modules/0-import_add.py
+++ python-import_modules/0-import_add.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 add = __import__("0-add").add
 
-a = 1
-b = 2
+if __name__ == "__main__":
+    a = 1
+    b = 2
 
-print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
+    print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
