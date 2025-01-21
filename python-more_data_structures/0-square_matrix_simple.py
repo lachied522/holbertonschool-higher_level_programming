f4aef3e124d8d6886e24739a2bcef18fc7876724
root <lachie.duncan12@gmail.com>
2025-01-21 02:13:48 +0000

    Fixed logic

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -1,10 +1,13 @@
 #!/usr/bin/python3
 
 def square_matrix_simple(matrix=[]):
-    new_matrix = [[0] * len(matrix[0])] * len(matrix)
+    new_matrix = [[]] * len(matrix)
 
     for i in range(len(matrix)):
         for j in range(len(matrix[i])):
-            new_matrix[i][j] = matrix[i][j] ** 2
+            if j == 0:
+                new_matrix[i] = [matrix[i][j] ** 2]
+            else:
+                new_matrix[i].append(matrix[i][j] ** 2)
 
     return new_matrix
