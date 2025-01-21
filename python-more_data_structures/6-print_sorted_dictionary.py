20121b8012451e7f3badcffcdb4e9d4279c57126
root <lachie.duncan12@gmail.com>
2025-01-21 02:33:46 +0000

    Update key in dictionary

--- python-more_data_structures/6-print_sorted_dictionary.py
+++ python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,7 +1,4 @@
 #!/usr/bin/python3
 
-def print_sorted_dictionary(a_dictionary):
-    sorted_keys = sorted(a_dictionary.keys())
-
-    for key in sorted_keys:
-        print(f"{key}: {a_dictionary[key]}")
+def update_dictionary(a_dictionary, key, value):
+    a_dictionary[key] = value
