15bade3e8adba9660c47bb060cbd5f8b384845a7
root <lachie.duncan12@gmail.com>
2025-01-22 22:58:03 +0000

    Fixed styling

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -1,15 +1,7 @@
 #!/usr/bin/python3
 
 def best_score(a_dictionary):
-    best_value = None
-    best_key = None
-
-    if a_dictionary == None:
+    if a_dictionary is None:
         return None
 
-    for key in a_dictionary:
-        if best_value == None or a_dictionary[key] > best_value:
-            best_value = a_dictionary[key]
-            best_key = key
-
-    return key
+    return max(a_dictionary, key=a_dictionary.get)
