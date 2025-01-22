9f988bc18002dd157325922768f2bd37ca0dc9b7
root <lachie.duncan12@gmail.com>
2025-01-22 23:21:21 +0000

    Convert roman number to int

---
+++ python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+
+char_map = {
+    "I": 1,
+    "V": 5,
+    "X": 10,
+    "L": 50,
+    "C": 100,
+    "D": 500,
+    "M": 1000
+}
+
+def roman_to_int(roman_string):
+    if type(roman_string) != str:
+        return 0
+
+    reversed_string = roman_string[::-1]
+
+    num = char_map[reversed_string[0]]
+    last_char = reversed_string[0]
+
+    for c in reversed_string[1:]:
+        if char_map[c] < char_map[last_char]:
+            num -= char_map[c]
+        else:
+            num += char_map[c]
+            last_char = c
+
+    return num
