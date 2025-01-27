aed13f2b9883366a9e5d87101a7d456ff5452f74
root <lachie.duncan12@gmail.com>
2025-01-27 23:38:10 +0000

    Fixed incorrect comparison

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -58,8 +58,8 @@ class Square:
             and len(value) == 2
             and isinstance(value[0], int)
             and isinstance(value[1], int)
-            and value[0] > 0
-            and value[1] > 0
+            and value[0] >= 0
+            and value[1] >= 0
         ):
             raise TypeError("position must be a tuple of 2 positive integers")
 
