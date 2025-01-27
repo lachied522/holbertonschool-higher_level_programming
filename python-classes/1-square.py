1ad72b7ce8a4380c2d19f346962a8ebc3718eb4c
root <lachie.duncan12@gmail.com>
2025-01-27 23:48:34 +0000

    Removed getter

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -19,23 +19,3 @@ class Square:
                               must be positive
         """
         self.size = size
-
-    @property
-    def size(self):
-        """
-        Getter for size property
-        """
-        return self.__size
-
-    @size.setter
-    def size(self, value):
-        """
-        Setter for size property
-        """
-        if not isinstance(value, int):
-            raise TypeError("size must be an integer")
-
-        if value < 0:
-            raise ValueError("size must be >= 0")
-
-        self.__size = value
