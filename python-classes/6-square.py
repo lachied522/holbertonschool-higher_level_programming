0460937a2079dd95702c27f657bb21c8c70d3eb9
root <lachie.duncan12@gmail.com>
2025-01-27 23:31:48 +0000

    Added documentation

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -1,17 +1,38 @@
 #!/usr/bin/python3
+"""
+This a module docstring
+"""
+
 
 class Square:
+    """
+    This is a class dosctring
+    """
 
-    def __init__(self, size=0, position=(0, 0)):    
+    def __init__(self, size=0, position=(0, 0)):
+        """
+        Initialise a Square instance with size and position
+
+        Args:
+            size (int): size of square, must be positive integer
+            position (tuple): coordinates of square in x y plane
+                              must be positive
+        """
         self.size = size
         self.position = position
 
     @property
     def size(self):
+        """
+        Getter for size property
+        """
         return self.__size
 
     @size.setter
     def size(self, value):
+        """
+        Setter for size property
+        """
         if not isinstance(value, int):
             raise TypeError("size must be an integer")
 
@@ -22,23 +43,35 @@ class Square:
 
     @property
     def position(self):
+        """
+        Getter for position property
+        """
         return self.__position
 
     @position.setter
     def position(self, value):
+        """
+        Setter for position property
+        """
         if not (
-                isinstance(value, tuple)
-                and isinstance(value[0], int)
-                and isinstance(value[1], int)
-            ):
+            isinstance(value, tuple)
+            and isinstance(value[0], int)
+            and isinstance(value[1], int)
+        ):
             raise TypeError("position must be a tuple of 2 positive integers")
 
         self.__position = value
 
     def area(self):
+        """
+        Return area of square
+        """
         return self.__size ** 2
 
     def my_print(self):
+        """
+        Print square in plane
+        """
         size = self.size
         x_start = self.position[0]
         y_start = self.position[1]
@@ -57,4 +90,3 @@ class Square:
             for j in range(size):
                 print("#", end="")
             print("")
-
