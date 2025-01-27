4f0306d8d11a05074160902f597e090a06581416
root <lachie.duncan12@gmail.com>
2025-01-27 23:37:15 +0000

    Added position argument validation

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -55,8 +55,11 @@ class Square:
         """
         if not (
             isinstance(value, tuple)
+            and len(value) == 2
             and isinstance(value[0], int)
             and isinstance(value[1], int)
+            and value[0] > 0
+            and value[1] > 0
         ):
             raise TypeError("position must be a tuple of 2 positive integers")
 
