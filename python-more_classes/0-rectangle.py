b5732527d6a37599f20797427f6f9f7d0e40ab43
root <lachie.duncan12@gmail.com>
2025-01-28 00:00:38 +0000

    Initiliase class

---
+++ python-more_classes/0-rectangle.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+This is a module docstring
+"""
+
+
+class Rectangle:
+    """
+    This is a class docstring
+    """
+    def __init__(self):
+        """
+        Initialiases an instance of this class
+        """
+        pass
