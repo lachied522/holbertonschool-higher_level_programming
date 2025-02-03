17de125bc535eb702b2f4d7af484f885e849ae7f
root <lachie.duncan12@gmail.com>
2025-02-03 22:38:23 +0000

    Adding a class method

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+This is a module docstring
+"""
+
+
+class BaseGeometry:
+    """
+    Custom list class that inherits from built in list
+    """
+
+    def __init__(self):
+        pass
+
+    def area(self):
+        raise Exception("area() is not implemented")
