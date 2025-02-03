6997a0352e7f9d83bf30726c8f5e4957a591b2e9
root <lachie.duncan12@gmail.com>
2025-02-03 22:36:58 +0000

    Empty class

---
+++ python-inheritance/5-base_geometry.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+This is a module docstring
+"""
+
+
+class BaseGeometry:
+    """
+    Custom list class that inherits from built in list
+    """
+
+    def __init__(self):
+        pass
