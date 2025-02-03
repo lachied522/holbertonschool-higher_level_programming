98ca015b8177ddc736c6ec959af438f8306d71d2
root <lachie.duncan12@gmail.com>
2025-02-03 22:23:49 +0000

    Added documentation

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -1,4 +1,11 @@
 #!/usr/bin/python3
+"""
+This is a module dosctring
+"""
+
 
 def lookup(obj):
+    """
+    Return list of available attributes of object
+    """
     return dir(obj())
--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -1,6 +1,16 @@
 #!/usr/bin/python3
+"""
+This is a module docstring
+"""
+
 
 class MyList(list):
+    """
+    Custom list class that inherits from built in list
+    """
 
     def print_sorted(self):
+        """
+        Print sorted items
+        """
         print(sorted(self))
