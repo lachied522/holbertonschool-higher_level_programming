d529458235c2270eef9c8fb3e78e5316475d56db
root <lachie.duncan12@gmail.com>
2025-02-09 04:47:31 +0000

    Class mixins

---
+++ python-abc/task_05_dragon.py
@@ -0,0 +1,24 @@
+"""
+This is module docstring
+"""
+#!/usr/bin/env python3
+
+class SwimMixin: 
+    """
+    This is a class docstring
+    """
+
+    def swim(self):
+        print("The creature swims!")
+
+class FlyMixin:
+    """"""
+
+    def fly(self):
+        print("The creature flies!")
+
+class Dragon(SwimMixin, FlyMixin):
+    """"""
+
+    def roar(self):
+        print("The creature roars!")
