639c4b267ad70f04cbb361bb212bf38453a6c374
Lachie <lachie.duncan12@gmail.com>
2025-02-17 23:44:39 +0000

    fixed info endpoint

--- restful-api/task_03_http_server.py
+++ restful-api/task_03_http_server.py
@@ -16,7 +16,7 @@ class Handler(BaseHTTPRequestHandler):
             self.end_headers()
             self.wfile.write(b"Hello, this is a simple API!")
 
-        elif self.path == "/data":
+        elif self.path == "/info":
             self.send_response(200)
             self.send_header("Content-type", "application/json")
             self.end_headers()
@@ -43,8 +43,7 @@ class Handler(BaseHTTPRequestHandler):
             self.send_response(404)
             self.send_header("Content-type", "application/json")
             self.end_headers()
-            error_message = {"error": "Endpoint not found"}
-            self.wfile.write(json.dumps(error_message).encode("utf-8"))
+            self.wfile.write(b"Endpoint not found")
 
 
 def run(server_class=HTTPServer, handler_class=Handler):
