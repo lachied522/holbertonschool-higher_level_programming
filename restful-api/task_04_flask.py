a2ca26d682cb710eb5b60651b2aed2dc1b1c634d
Lachie <lachie.duncan12@gmail.com>
2025-02-24 00:27:06 +0000

    changed status code for user not found

--- restful-api/task_04_flask.py
+++ restful-api/task_04_flask.py
@@ -30,7 +30,7 @@ def get_username(username: str):
     data = USERS.get(username, None)
 
     if data is None:
-        return { "error": "User not found" }, 400
+        return { "error": "User not found" }, 404
 
     return jsonify(data)
 
