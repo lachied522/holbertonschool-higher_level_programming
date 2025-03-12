c02a7fe713d162e2a8709885f6a4f6ac7eb580a8
Lachie <lachie.duncan12@gmail.com>
2025-03-12 05:48:56 +0000

    Switched to left join

--- SQL_more_queries/16-shows_by_genre.sql
+++ SQL_more_queries/16-shows_by_genre.sql
@@ -1,6 +1,6 @@
 -- List all shows and genres
 SELECT tv_shows.title, tv_genres.name
 FROM tv_shows
-JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
-JOIN tv_genres ON tv_genres.id = tv_show_genres.genre_id
+LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+LEFT JOIN tv_genres ON tv_genres.id = tv_show_genres.genre_id
 ORDER BY tv_shows.title, tv_genres.name;
