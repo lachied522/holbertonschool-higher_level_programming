4fc2e65f06ab180381e727223bf1e3a418de8fe9
Lachie <lachie.duncan12@gmail.com>
2025-03-12 05:27:03 +0000

    Fixed case of SQL keyword

--- SQL_more_queries/13-count_shows_by_genre.sql
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,6 +1,6 @@
 -- Select shows order with no genre
-SELECT tv_show_genres.name as genre,
-    COUNT(*) as number_of_shows
+SELECT tv_show_genres.name AS genre,
+    COUNT(*) AS number_of_shows
         FROM tv_shows
         LEFT JOIN tv_show_genres
         ON tv_shows.id = tv_show_genres.show_id
