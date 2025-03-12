87c34b01c2205d5088be3bd5768dc481f106612a
Lachie <lachie.duncan12@gmail.com>
2025-03-12 05:43:26 +0000

    Fixed return column

--- SQL_more_queries/15-comedy_only.sql
+++ SQL_more_queries/15-comedy_only.sql
@@ -1,5 +1,5 @@
 -- Select shows in comedy genre
-SELECT tv_genres.name AS genre
+SELECT tv_shows.title
 FROM tv_shows
 JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
 JOIN tv_genres ON tv_genres.id = tv_show_genres.genre_id
